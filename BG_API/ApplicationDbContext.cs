ee32aee22805b622593eeda89271c6f829b883e5
Jhonatan Moises <chahuayoboza@gmail.com>
2022-04-09 10:58:13 -0500

    Agregar archivos de proyecto.

---
+++ BG_API/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BG_API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BG_API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
